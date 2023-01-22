b0a37422ee2f2bae97fb437d4e73c8b0dbf5d7b6
betulnecanli <betulnecanli@gmail.com>
2023-01-22 21:28:06 +0300

    Dependency Injection with DaggerHilt Initialization

---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/SailorMoonApp.kt
@@ -0,0 +1,8 @@
+package com.betulnecanli.sailormoonapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SailorMoonApp : Application() {
+}
---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.betulnecanli.sailormoonapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+
+
+
+}
