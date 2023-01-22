53b654258cbaa040150788c0e37305db90f336f2
betulnecanli <betulnecanli@gmail.com>
2023-01-22 21:40:49 +0300

    Fragments added

---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/ui/detail/DetailFragment.kt
@@ -0,0 +1,28 @@
+package com.betulnecanli.sailormoonapp.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.betulnecanli.sailormoonapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+
+}
